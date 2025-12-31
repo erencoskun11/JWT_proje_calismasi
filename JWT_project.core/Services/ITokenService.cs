4f945746f0078dab99b2e4ad240487197206a6a7
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2026-01-01 00:04:55 +0300

    feat(interface) : created

---
+++ JWT_project.core/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using JWT_project.core.Configuration;
+using JWT_project.core.DTOs;
+using JWT_project.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT_project.core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateTokenDto(UserApp userApp);
+
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
